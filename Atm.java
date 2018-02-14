d03ed4e5867e8c972c1f86f73719de0f3e62c8dc
Meet Kumar Vishwakarma <kumarmeet51@yahoo.com>
2018-02-14 23:32:36 +0530

    password protected
    user input password then enter the atm machine password is 1234

--- Atm.java
+++ Atm.java
@@ -1,9 +1,19 @@
 package includeatm;
 import atm.Meet;
+import java.util.*;
 class Atm
 {
     public static void main(String []args)
     {
-        Meet.check();
+        System.out.print("Enter Your Password: ");
+        Scanner scanner=new Scanner(System.in);
+        int password=scanner.nextInt();
+        if(password==1234)
+        {
+            Meet.check();
+        }
+        else{
+            System.out.println("Wrong Input");
+        }
     }
 }
